f85659d885be18970240f4c8e29907205d0c4cbd
Sohan Basak <ronnie.basak96@gmail.com>
2020-09-13 13:33:28 +0530

    Chessboard ready

---
+++ src/components/Chess.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="chessboard">
+      <template v-for="i in 8">
+          <template v-for="j in 8"> 
+          <div class="chessbox-square" :key="`sq_${i}_${j}`" :id="`sq_${(9-i)*8+j}`">
+            {{(8-i)*8+j}}
+          </div>
+          </template>
+      </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chess",
+
+  methods: {
+      changeAddressSpace(i){
+          return i
+      }
+  }
+};
+</script>
+
+
+<style scoped lang="scss">
+.chessboard {
+    width: 80vmin;
+    height: 80vmin;
+    background: transparent;
+}
+
+.chessbox-square {
+    background: black;
+    height: 10vmin;
+    width: 10vmin;
+    display: inline-block;
+    color: blue;
+}
+
+.chessbox-square:nth-child(16n+1),
+.chessbox-square:nth-child(16n+3),
+.chessbox-square:nth-child(16n+5),
+.chessbox-square:nth-child(16n+7),
+.chessbox-square:nth-child(16n+10),
+.chessbox-square:nth-child(16n+12),
+.chessbox-square:nth-child(16n+14),
+.chessbox-square:nth-child(16n+16)
+ {
+    background: white;
+}
+
+
+</style>
